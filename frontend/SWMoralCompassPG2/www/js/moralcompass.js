e548170ed2ab03be233c99f5ed5ec491d1d45bb3
will <>
2012-08-18 14:58:23 -0700

    more stuff rigged to moralcompass.js, bottom buttons as list right now in index.html

--- frontend/SWMoralCompassPG2/www/js/moralcompass.js
+++ frontend/SWMoralCompassPG2/www/js/moralcompass.js
@@ -7,13 +7,44 @@ MCApp.getUrlForAPI = function(){
     return "http://127.0.0.1/";
 }
 
+MCApp.currentBarcode = false;
+
 /**
- *  Takes a barcode and calls a handler.
+ *  Scans a barcode and calls a handler.
  *  
  *  void -> void
  */
-MCApp.takeBarcode = function(){
-    throw "unimplemented";
+MCApp.scanBarcode = function(){
+   MCApp.scanBarcodeWithScandit();
+}
+
+MCApp.getCurrentBarcode = function(){
+    return MCApp.currentBarcode;
+}
+
+MCApp.scanBarcodeWithScandit = function(){
+     ScanditSDK.nativeFunction(MCApp.scanBarcodeSuccessScandit,
+                              MCApp.scanBarcodeFailScandit,
+                              "nIGuaOlnEeGXNUEZL6NnwOyanGE2nEdWOtIrkDGfuVs", {
+                                          "beep": true, 
+                                          "1DScanning" : true, 
+                                          "2DScanning" : true, 
+                                          "scanningHotspot" : "0.5/0.5", 
+                                          "vibrate" : true, 
+                                          "showMostLikelyBarcodeUIElement" : false,
+                                          "textForMostLikelyBarcodeUIElement" : "Most likely barcode",
+                                          "textForInitialScanScreenState" : "Align code with box",
+                                          "textForBarcodePresenceDetected" : "Align code and hold still",
+                                          "textForBarcodeDecodingInProgress" : "Decoding",
+                                          "searchBarActionButtonCaption" : "Go",
+                                          "searchBarCancelButtonCaption" : "Cancel",
+                                          "searchBarPlaceholderText" : "Scan barcode or enter it here",
+                                          "toolBarButtonCaption" : "Cancel",
+                                          "viewfinderColor" : "FFFFFF",
+                                          "viewfinderDecodedColor" : "00FF00",
+                                          "minSearchBarBarcodeLength" : 8,
+                                          "maxSearchBarBarcodeLength" : 15
+                                          });
 }
 
 /**
@@ -21,26 +52,53 @@ MCApp.takeBarcode = function(){
  *  
  *  void -> 
  */
-MCApp.takeBarcodeSuccess = function(){
-    throw "unimplemented";    
+MCApp.scanBarcodeSuccessScandit = function(concatResult){
+    var resultArray = concatResult.split("|"); 
+    MCApp.currentBarcode = resultArray[1];
 }
 
 /**
  *  Function to handle barcode scanning failure.
  *  
- *  void -> 
+ *  void -> false
  */
-MCApp.takeBarcodeFail = function(){
-    throw "unimplemented";
+MCApp.scanBarcodeFailScandit = function(){
+    MCApp.currentBarcode = false;
 }
 
 /**
  *  Produces a company name from a barcode result.
  *  
- *  string -> 
+ *  string -> string
  */
-MCApp.getCompanyNameFromBarcode = function(barcode){
-    throw "unimplemented";
+MCApp.getCompanyNameFromBarcode = function(barcodeStr){
+    var result = MCApp.getCompanyNameFromBarcodeLocal(barcodeStr);
+    
+    if(result == "unknown"){
+        result = MCApp.getCompanyNameFromBarcodeRemote(barcodeStr);
+    }
+    
+    return result;
+}
+
+/**
+ *  Produces a locally-stored company name or "unknown"
+ *  
+ *  string -> string
+ */
+MCApp.getCompanyNameFromBarcodeLocal = function(barcodeStr){
+    var code = $.trim(barcodeStr);
+    if(code == "065633506657") return "Nature Valley";
+    else return "unknown";
+}
+
+/**
+ *  Produces a company name from an external database, or "unknown"
+ *  
+ *  string -> string
+ */
+MCApp.getCompanyNameFromBarcodeRemote = function(barcodeStr){
+    return "unknown";
 }
 
 /**
@@ -162,6 +220,11 @@ MCBeliefs.prototype.save = function(){
     return true;
 }
 
+/**
+ *  Produces the currently stored array of stances.
+ *  
+ *  void -> array(<MCStance>)
+ */
 MCBeliefs.prototype.getStances = function(){
     return this.arrayOfBeliefs;
 }
@@ -214,6 +277,35 @@ MCBeliefs.prototype.getNumAgreeToArrayOfCompanyStances = function(stancesArray){
     return sumMatch;
 }
 
+/**
+ *  Produces the string representation of the array of stances.
+ * 
+ *  void -> string
+ */
 MCBeliefs.prototype.toString = function(){
     return JSON.stringify(this.arrayOfBeliefs);
+}
+
+/**
+ *  Test data generator.
+ */
+MCTest = function(){
+    
+}
+
+/**
+ *  Produces deterministic test stances from a barcode
+ * 
+ *  barcode -> array(<MCStance>)
+ */
+MCTest.prototype.getTestStancesFromBarcode = function(barcode){
+    var size = MCBeliefDictionary.getNumBeliefs();
+    var numCode = parseInt(barcode);
+    var toyData = [];
+    
+    for(var i=0; i<size; i++){
+        toyData[i] = Math.floor(numCode/(i+1)) % 3;
+    }
+    
+    return toyData;
 }
